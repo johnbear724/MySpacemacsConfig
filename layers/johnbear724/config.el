1b88bb5bb30574a2633bc54103cc717f9970c9b9
johnbear724 <johnbear724@gmail.com>
2017-10-25 23:12:45 +0800

    Add dired sorting for linux.

--- layers/johnbear724/config.el
+++ layers/johnbear724/config.el
@@ -92,3 +92,16 @@
 ;;   )
 
 (setq ls-lisp-dirs-first t)
+
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+	(after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
